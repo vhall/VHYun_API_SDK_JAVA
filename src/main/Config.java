bf30a143f42696d4760eeb50d3692acf0c952338
GaoYan <yan.gao@vhall.com>
2018-04-23 18:10:24 +0800

    fix

--- src/main/Config.java
+++ src/main/Config.java
@@ -12,8 +12,8 @@ public class Config {
     {
         HashMap<String, Object> configMap = new HashMap<String, Object>();
 
-        configMap.put("app_id", "342a10bb");
-        configMap.put("secret_key", "7b887d97f11cb1f7c6cb890fbecf0367");
+        configMap.put("app_id", "xxxxxxxxxx");
+        configMap.put("secret_key", "xxxxxxxxxxxxx");
         //configMap.put("request_domain", "http://api.vhallyun.com/api/v1");
 
         return configMap;
